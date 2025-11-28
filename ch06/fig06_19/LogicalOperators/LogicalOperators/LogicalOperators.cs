3e996d1396d1d0d131a546c23c1099495314acfb
Emrullah Altay <50184111+MrAltay@users.noreply.github.com>
2025-11-28 15:04:18 +0300

    Add files via upload

---
+++ ch06/fig06_19/LogicalOperators/LogicalOperators/LogicalOperators.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+class MantiksalOperatorler
+{
+   static void Main()
+   {
+      Console.WriteLine("Koşullu VE (&&)");
+      Console.WriteLine($"false && false: {false && false}");
+      Console.WriteLine($"false && true: {false && true}");
+      Console.WriteLine($"true && false: {true && false}");
+      Console.WriteLine($"true && true: {true && true}\n");
+
+      Console.WriteLine("Koşullu VEYA (||)");
+      Console.WriteLine($"false || false: {false || false}");
+      Console.WriteLine($"false || true: {false || true}");
+      Console.WriteLine($"true || false: {true || false}");
+      Console.WriteLine($"true || true: {true || true}\n");
+
+      Console.WriteLine("Mantıksal VE (&)");
+      Console.WriteLine($"false & false: {false & false}");
+      Console.WriteLine($"false & true: {false & true}");
+      Console.WriteLine($"true & false: {true & false}");
+      Console.WriteLine($"true & true: {true & true}\n");
+
+      Console.WriteLine("Mantıksal Kapsayıcı VEYA (|)");
+      Console.WriteLine($"false | false: {false | false}");
+      Console.WriteLine($"false | true: {false | true}");
+      Console.WriteLine($"true | false: {true | false}");
+      Console.WriteLine($"true | true: {true | true}\n");
+
+      Console.WriteLine("Mantıksal Özel VEYA (^)");
+      Console.WriteLine($"false ^ false: {false ^ false}");
+      Console.WriteLine($"false ^ true: {false ^ true}");
+      Console.WriteLine($"true ^ false: {true ^ false}");
+      Console.WriteLine($"true ^ true: {true ^ true}\n");
+
+      Console.WriteLine("Mantıksal Değil (!)");
+      Console.WriteLine($"!false: {!false}");
+      Console.WriteLine($"!true: {!true}");
+   }
+}
