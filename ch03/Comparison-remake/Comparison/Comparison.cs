5d10724005805351e59c6e56fab586e895b32ae3
Emrullah Altay <50184111+MrAltay@users.noreply.github.com>
2025-11-28 15:03:39 +0300

    Add files via upload

---
+++ ch03/Comparison-remake/Comparison/Comparison.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+class Karsilastirma
+{
+   static void Main()
+   {
+      Console.Write("Birinci tamsayıyı girin: ");
+      int sayi1 = int.Parse(Console.ReadLine());
+
+      Console.Write("İkinci tamsayıyı girin: ");
+      int sayi2 = int.Parse(Console.ReadLine());
+
+      if (sayi1 == sayi2)
+      {
+         Console.WriteLine($"{sayi1} == {sayi2}");
+      }
+
+      if (sayi1 != sayi2)
+      {
+         Console.WriteLine($"{sayi1} != {sayi2}");
+      }
+
+      if (sayi1 < sayi2)
+      {
+         Console.WriteLine($"{sayi1} < {sayi2}");
+      }
+
+      if (sayi1 > sayi2)
+      {
+         Console.WriteLine($"{sayi1} > {sayi2}");
+      }
+
+      if (sayi1 <= sayi2)
+      {
+         Console.WriteLine($"{sayi1} <= {sayi2}");
+      }
+
+      if (sayi1 >= sayi2)
+      {
+         Console.WriteLine($"{sayi1} >= {sayi2}");
+      }
+   }
+}
