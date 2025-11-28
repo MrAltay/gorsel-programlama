c0291d32762fda3d7629ac30e8fc85e1fa3a5dcb
Emrullah Altay <50184111+MrAltay@users.noreply.github.com>
2025-11-28 15:04:58 +0300

    Add files via upload

---
+++ ch10/fig10_14-15/OperatorOverloading/OperatorOverloading/ComplexNumber.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+public struct KarmasikSayi
+{
+   public double Gercel { get; }
+   public double Sanal { get; }
+
+   public KarmasikSayi(double gercel, double sanal)
+   {
+      Gercel = gercel;
+      Sanal = sanal;
+   }
+
+   public override string ToString() =>
+      $"({Gercel} {(Sanal < 0 ? "-" : "+")} {Math.Abs(Sanal)}i)";
+
+   public static KarmasikSayi operator+(KarmasikSayi x, KarmasikSayi y)
+   {
+      return new KarmasikSayi(x.Gercel + y.Gercel,
+         x.Sanal + y.Sanal);
+   }
+
+   public static KarmasikSayi operator-(KarmasikSayi x, KarmasikSayi y)
+   {
+      return new KarmasikSayi(x.Gercel - y.Gercel,
+         x.Sanal - y.Sanal);
+   }
+
+   public static KarmasikSayi operator*(KarmasikSayi x, KarmasikSayi y)
+   {
+      return new KarmasikSayi(
+         x.Gercel * y.Gercel - x.Sanal * y.Sanal,
+         x.Gercel * y.Sanal + y.Gercel * x.Sanal);
+   }
+}
---
+++ ch10/fig10_16/TimeExtensions/TimeExtensions/TimeExtensionsTest.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+class ZamanGenisletmeleriTesti
+{
+   static void Main()
+   {
+      var zamanim = new Zaman2(); 
+      zamanim.ZamanAyarla(11, 34, 15); 
+
+      Console.Write("ZamaniGoster genişletme metodunu kullan: ");
+      zamanim.ZamaniGoster();
+
+      Console.Write("SaatEkle genişletme metodu ile 5 saat ekle: ");
+      var eklenmisZaman = zamanim.SaatEkle(5); 
+      eklenmisZaman.ZamaniGoster(); 
+
+      Console.Write("SaatEkle genişletme metodu ile 15 saat ekle: ");
+      zamanim.SaatEkle(15).ZamaniGoster(); 
+
+      Console.Write("Tam nitelikli genişletme metodu adını kullan: ");
+      ZamanGenisletmeleri.ZamaniGoster(zamanim);
+   }
+}
+
+static class ZamanGenisletmeleri
+{
+   public static void ZamaniGoster(this Zaman2 birZaman)
+   {
+      Console.WriteLine(birZaman.ToString());
+   }
+
+   public static Zaman2 SaatEkle(this Zaman2 birZaman, int saatler)
+   {
+      var yeniZaman = new Zaman2() {
+         Dakika = birZaman.Dakika, Saniye = birZaman.Saniye};
+
+      yeniZaman.Saat = (birZaman.Saat + saatler) % 24;
+
+      return yeniZaman; 
+   }
+}
