b8304ba812208d950ffe826120ddbaabaf8d60da
Emrullah Altay <50184111+MrAltay@users.noreply.github.com>
2025-12-26 16:18:26 +0300

    Add files via upload

---
+++ 11. Ünite/Fig11_08/BasePlusCommissionEmployee/BasePlusCommissionEmployee/BasePlusCommissionEmployee.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+public class TabanMaasliKomisyonluCalisan : KomisyonluCalisan
+{
+   private decimal tabanMaas;
+
+   public TabanMaasliKomisyonluCalisan(string ad, string soyad,
+      string sosyalGuvenlikNo, decimal brutSatis,
+      decimal komisyonOrani, decimal tabanMaas)
+      : base(ad, soyad, sosyalGuvenlikNo,
+           brutSatis, komisyonOrani)
+   {
+      TabanMaas = tabanMaas;
+   }
+
+   public decimal TabanMaas
+   {
+      get
+      {
+         return tabanMaas;
+      }
+      set
+      {
+         if (value < 0)
+         {
+            throw new ArgumentOutOfRangeException(nameof(value),
+               value, $"{nameof(TabanMaas)} değeri >= 0 olmalıdır");
+         }
+
+         tabanMaas = value;
+      }
+   }
+
+   public override decimal Kazanc() =>
+      tabanMaas + (komisyonOrani * brutSatis);
+
+   public override string ToString() =>
+      $"taban maaşlı komisyonlu çalışan: {Ad} {Soyad}\n" +
+      $"sosyal güvenlik numarası: {SosyalGuvenlikNo}\n" +
+      $"brüt satış: {brutSatis:C}\n" +
+      $"komisyon oranı: {komisyonOrani:F2}\n" +
+      $"taban maaş: {tabanMaas}";
+}
---
+++ 11. Ünite/Fig11_08/BasePlusCommissionEmployee/BasePlusCommissionEmployee/CommissionEmployee.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+public class KomisyonluCalisan : object
+{
+   public string Ad { get; }
+   public string Soyad { get; }
+   public string SosyalGuvenlikNo { get; }
+   private decimal brutSatis; 
+   private decimal komisyonOrani; 
+
+   public KomisyonluCalisan(string ad, string soyad,
+      string sosyalGuvenlikNo, decimal brutSatis,
+      decimal komisyonOrani)
+   {
+      Ad = ad;
+      Soyad = soyad;
+      SosyalGuvenlikNo = sosyalGuvenlikNo;
+      BrutSatis = brutSatis; 
+      KomisyonOrani = komisyonOrani; 
+   }
+
+   public decimal BrutSatis
+   {
+      get
+      {
+         return brutSatis;
+      }
+      set
+      {
+         if (value < 0) 
+         {
+            throw new ArgumentOutOfRangeException(nameof(value),
+               value, $"{nameof(BrutSatis)} değeri >= 0 olmalıdır");
+         }
+
+         brutSatis = value;
+      }
+   }
+
+   public decimal KomisyonOrani
+   {
+      get
+      {
+         return komisyonOrani;
+      }
+      set
+      {
+         if (value <= 0 || value >= 1) 
+         {
+            throw new ArgumentOutOfRangeException(nameof(value),
+               value, $"{nameof(KomisyonOrani)} değeri > 0 ve < 1 olmalıdır");
+         }
+
+         komisyonOrani = value;
+      }
+   }
+
+   public decimal Kazanc() => komisyonOrani * brutSatis;
+
+   public override string ToString() =>
+      $"komisyonlu çalışan: {Ad} {Soyad}\n" +
+      $"sosyal güvenlik numarası: {SosyalGuvenlikNo}\n" +
+      $"brüt satış: {brutSatis:C}\n" +
+      $"komisyon oranı: {komisyonOrani:F2}";
+}
